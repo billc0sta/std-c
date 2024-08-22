27b293693b1b4daf639c2e6db429014fca413027
Mohamed <billcosta@gmail.com>
2024-08-22 22:15:24 +0300

    updating math.h

--- stdlibc.h
+++ stdlibc.h
@@ -35,9 +35,167 @@ long double fabsl(long double arg) {
 	return -arg;
 }
 
-float fmod(float x, float y) {
-	if ()
+float fmodf(float x, float y) {
+	if (isnan(x) || isnan(y))
+		return NAN;
+	if (!isfinite(x) || y == 0) {
+		// -> TODO: requires fenv.h 
+		fexcept |= FE_INVALID;
+		return NAN
+	}
+	return x % y;
 }
+
+double fmod(double x, double y) {
+	if (isnan(x) || isnan(y))
+		return NAN;
+	if (!isfinite(x) || y == 0) {
+		// -> TODO: requires fenv.h 
+		fexcept |= FE_INVALID;
+		return NAN
+	}
+	return x % y;
+}
+
+long double fmodl(long double x, long double y) {
+	if (isnan(x) || isnan(y))
+		return NAN;
+	if (!isfinite(x) || y == 0) {
+		// -> TODO: requires fenv.h 
+		fexcept |= FE_INVALID;
+		return NAN
+	}
+	return x % y;
+}
+
+float remainderf(float x, float y) {
+	if (isnan(x) || isnan(y))
+		return NAN;
+	if (!isfinite(x) || y == 0) {
+		// -> TODO: requires fenv.h 
+		fexcept |= FE_INVALID;
+		return NAN
+	}
+	return x - (x/y) * y;
+}
+
+double remainder(double x, double y) {
+	if (isnan(x) || isnan(y))
+		return NAN;
+	if (!isfinite(x) || y == 0) {
+		// -> TODO: requires fenv.h 
+		fexcept |= FE_INVALID;
+		return NAN
+	}
+	return x - (x/y) * y;
+}
+
+long double remainderl(long double x, long double y) {
+	if (isnan(x) || isnan(y))
+		return NAN;
+	if (!isfinite(x) || y == 0) {
+		// -> TODO: requires fenv.h 
+		fexcept |= FE_INVALID;
+		return NAN
+	}
+	return x - (x/y) * y;
+}
+
+float remquof(float x, float y, int *quo) {
+	if (isnan(x) || isnan(y))
+		return NAN;
+	if (!isfinite(x) || y == 0) {
+		// -> TODO: requires fenv.h 
+		fexcept |= FE_INVALID;
+		return NAN
+	}
+	float divd = (x/y);
+	*quo = 0; 
+	*quo |= (divd & (1 << sizeof(divd) * 8 - 1));
+	*quo |= (divd & ((1 << 3) - 1));
+	return x - divd * y;
+}
+
+double remquo(double x, double y, int *quo) {
+	if (isnan(x) || isnan(y))
+		return NAN;
+	if (!isfinite(x) || y == 0) {
+		// -> TODO: fexcept and FE_INVALID requires fenv.h 
+		fexcept |= FE_INVALID;
+		return NAN
+	}
+	double divd = (x/y);
+	*quo = 0; 
+	*quo |= (divd & (1 << sizeof(divd) * 8 - 1));
+	*quo |= (divd & (double)((1 << 3) - 1)) << sizeof(divd) - sizeof(qou);
+	return x - divd * y;
+}
+
+long double remquol(long double x, long double y, int *quo) {
+	if (isnan(x) || isnan(y))
+		return NAN;
+	if (!isfinite(x) || y == 0) {
+		// -> TODO: requires fenv.h 
+		fexcept |= FE_INVALID;
+		return NAN
+	}
+	long double divd = (x/y);
+	*quo = 0; 
+	*quo |= (divd & (1 << sizeof(divd) * 8 - 1));
+	*quo |= (divd & (double)((1 << 3) - 1)) << sizeof(divd) - sizeof(qou);
+	return x - divd * y;
+}
+
+// -> TODO: fma()
+
+float fmaxf(float x, float y) {
+	return (x > y) ? x : y;
+}
+
+double fmax(double x, double y) {
+	return (x > y) ? x : y;
+}
+
+long double fmaxl(long double x, long double y) {
+	return (x > y) ? x : y;
+}
+
+float fminf(float x, float y) {
+	return (x < y) ? x : y;
+}
+
+double fmin(double x, double y) {
+	return (x < y) ? x : y;
+}
+
+long double fminl(long double x, long double y) {
+	return (x < y) ? x : y;
+}
+
+float fdimf(float x, float y) {
+	return fmaxf(0.f, x - y);
+}
+
+double fdim(double x, double y) {
+	return fmax(0.f, x - y);
+}
+
+long double fdimf(long double x, long double y) {
+	return fmaxl(0.f, x - y);
+}
+
+float nanf() {
+	// -> TODO: requires macro def of NAN
+	return (float)NAN;
+} 
+
+double nan() {
+	return (double)NAN;
+} 
+
+long double nanl() {
+	return (long double)NAN;
+} 
 
 // -> TODO: Complete math.h 
 
